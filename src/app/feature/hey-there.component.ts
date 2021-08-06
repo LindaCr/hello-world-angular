bbfbe70c2d607cf63a4700a00604bbee3af8c5ac
LindaCr <lindacrook.44@gmail.com>
2021-08-05 21:59:28 -0400

    init commit

---
+++ src/app/feature/hey-there.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hey-there',
+  templateUrl: './hey-there.component.html',
+  styleUrls: ['./hey-there.component.css']
+})
+export class HeyThereComponent implements OnInit {
+
+
+  title: string= "Hey There Component!"
+  username: string= "Enter username:";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.title+" was called");
+  }
+
+}
